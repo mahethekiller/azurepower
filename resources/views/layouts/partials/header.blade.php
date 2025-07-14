f69c826cf93149d6b75360404770510898f16beb
mahethekiller <abc@gmail.com>
2025-07-14 18:41:59 +0530

    first commit 4

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -13,6 +13,38 @@
     </div>
     <!-- Magic Cursor End -->
 
+    	<!-- Topbar Section Start -->
+	<div class="topbar wow fadeInUp">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-md-8">
+					<!-- Topbar Contact Information Start -->
+					<div class="topbar-contact-info">
+						<ul>
+							<li><a href="mailto:ir@azurepower.com"><i class="fa-solid fa-envelope"></i> ir@azurepower.com</a></li>
+							<li><a href="tel:+911244155755"><i class="fa-solid fa-phone"></i> +91 124-4155755</a></li>
+						</ul>
+					</div>
+					<!-- Topbar Contact Information End -->
+				</div>
+
+				<div class="col-md-4">
+					<!-- Topbar Social Links Start -->
+					<div class="header-social-links">
+						<ul>
+							<li><a href="#"><i class="fa-brands fa-facebook-f"></i></a></li>
+							<li><a href="#"><i class="fa-brands fa-twitter"></i></a></li>
+							<li><a href="#"><i class="fa-brands fa-linkedin-in"></i></a></li>
+							<li><a href="#"><i class="fa-brands fa-instagram"></i></a></li>
+						</ul>
+					</div>
+					<!-- Topbar Social Links End -->
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- Topbar Section End -->
+
     <!-- Header Start -->
 	<header class="main-header">
 		<div class="header-sticky">
