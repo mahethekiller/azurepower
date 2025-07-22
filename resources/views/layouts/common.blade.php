00687c8e3b4d70e899c9d64bfeae96ea92429695
mahethekiller <abc@gmail.com>
2025-07-22 18:38:24 +0530

    about page

--- resources/views/layouts/common.blade.php
+++ resources/views/layouts/common.blade.php
@@ -93,7 +93,28 @@
     <script src="{{ asset('assets/js/function.js') }}"></script>
     <script src="{{ asset('assets/js/owl.carousel.js') }}"></script>
 
-
+    <script>
+        $(document).ready(function() {
+            $(".owl-carousel").owlCarousel({
+                items: 3,
+                margin: 10,
+                loop: true,
+                autoplay: true,
+                autoplayTimeout: 3000,
+                responsive: {
+                    0: {
+                        items: 1
+                    },
+                    600: {
+                        items: 2
+                    },
+                    1000: {
+                        items: 3
+                    }
+                }
+            });
+        });
+    </script>
     @if ($pageScript)
         <script src="{{ asset('assets/customjs/' . $pageScript . '.js') }}"></script>
     @endif
